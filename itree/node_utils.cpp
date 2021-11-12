9579747da7743a9c0e8cdcfd788f5f46e7c1cd6a
Juncong Moo <juncongmoo@gmail.com>
2021-11-12 18:50:27 +0000

    node graph

--- itree/node_utils.cpp
+++ itree/node_utils.cpp
@@ -1,37 +1,38 @@
 #include "node_utils.h"
 #include "common.h"
-#include "shared.h"
 #include "fmt.h"
+#include "shared.h"
 
 static const double inf = std::numeric_limits<double>::infinity();
 
 shared_ptr<Node> create_virtual_node() {
-    py::dict m = {};
-    return make_shared<Node>(VNODE_NAME, inf, inf, m);
+  py::dict m = {};
+  return make_shared<Node>(VNODE_NAME, inf, inf, m);
 }
 
 shared_ptr<Node> create_tmp_node() {
-    py::dict m = {};
-    return make_shared<Node>("", 0, 0, m);
+  py::dict m = {};
+  return make_shared<Node>("", 0, 0, m);
 }
 
 string str_to_lower(const string &s) {
-    string data(s);
-    transform(data.begin(), data.end(), data.begin(), [](unsigned char c) { return tolower(c); });
-    return data;
+  string data(s);
+  transform(data.begin(), data.end(), data.begin(),
+            [](unsigned char c) { return tolower(c); });
+  return data;
 }
 
 bool is_virtual_node(const shared_ptr<Node> &n) {
-    // auto a = to_string(n->start);
-    // auto b = to_string(n->end);
-    // string lower_name = str_to_lower(n->name);
-    return ((n->start == inf and n->end == inf) or (n->start == -inf and n->end == inf) or
-            (n->start == inf and n->end == -inf));
+  // auto a = to_string(n->start);
+  // auto b = to_string(n->end);
+  // string lower_name = str_to_lower(n->name);
+  return ((n->start == inf and n->end == inf) or
+          (n->start == -inf and n->end == inf) or
+          (n->start == inf and n->end == -inf));
 }
 
-/*void __parse_node(const string &s, double &start, double &end, py::dict &extra) {
-    VS v = split(s, ",");
-    assert(v.size() == 3);
+/*void __parse_node(const string &s, double &start, double &end, py::dict
+&extra) { VS v = split(s, ","); assert(v.size() == 3);
 
     start = stod(v[0]);
     end = stod(v[1]);
@@ -55,8 +56,8 @@ shared_ptr<Node> deserialize(const string &bs) {
                 if (!s.empty()) {
                     auto kv = split(s, ":");
                     stk.back()->name = kv[0];
-                    __parse_node(kv[1], stk.back()->start, stk.back()->end, stk.back()->extra);
-                    s = "";
+                    __parse_node(kv[1], stk.back()->start, stk.back()->end,
+stk.back()->extra); s = "";
                 }
                 shared_ptr<Node> new_node = create_tmp_node();
                 stk.back()->nodes.push_back(new_node);
@@ -66,8 +67,8 @@ shared_ptr<Node> deserialize(const string &bs) {
             if (!s.empty()) {
                 auto kv = split(s, ":");
                 stk.back()->name = kv[0];
-                __parse_node(kv[1], stk.back()->start, stk.back()->end, stk.back()->extra);
-                s = "";
+                __parse_node(kv[1], stk.back()->start, stk.back()->end,
+stk.back()->extra); s = "";
             }
             if (stk.size() > 1) {
                 stk.pop_back();
@@ -81,31 +82,30 @@ shared_ptr<Node> deserialize(const string &bs) {
 }*/
 
 shared_ptr<Node> consolidate(const shared_ptr<Node> &node) {
-    shared_ptr<Node> n(node);
-    while (is_virtual_node(n)) {
-        if (n->nodes.size() == 1 and is_virtual_node(n->nodes.front())) {
-            n = n->nodes.front();
-        } else {
-            break;
-        }
+  shared_ptr<Node> n(node);
+  while (is_virtual_node(n)) {
+    if (n->nodes.size() == 1 and is_virtual_node(n->nodes.front())) {
+      n = n->nodes.front();
+    } else {
+      break;
     }
+  }
 
-    if (is_virtual_node(n) and !n->nodes.empty()) {
-        for (const auto &e : n->nodes) {
-            if (n->start == inf or n->start == -inf) {
-                n->start = e->start;
-            } else {
-                n->start = min(n->start, e->start);
-            }
-            if (n->end == inf or n->end == -inf) {
-                n->end = e->end;
-            } else
-                n->end = max(n->end, e->end);
-        }
+  if (is_virtual_node(n) and !n->nodes.empty()) {
+    for (const auto &e : n->nodes) {
+      if (n->start == inf or n->start == -inf) {
+        n->start = e->start;
+      } else {
+        n->start = min(n->start, e->start);
+      }
+      if (n->end == inf or n->end == -inf) {
+        n->end = e->end;
+      } else
+        n->end = max(n->end, e->end);
     }
-    return n;
+  }
+  return n;
 }
-
 
 void __dfs(const shared_ptr<Node> &n, unordered_set<string> &x,
            unordered_map<string, VS> &y, string &max_leaf_node_name,
@@ -123,28 +123,19 @@ void __dfs(const shared_ptr<Node> &n, unordered_set<string> &x,
   }
 }
 
-
 /*
 digraph tree{
-        node [shape=record margin=0 fontcolor=blue fontsize=8 width=0.5 style=filled fixedsize=true]
-        edge [fontsize=7]
-        rankdir=LR
+        node [shape=record margin=0 fontcolor=blue fontsize=8 width=0.5
+style=filled fixedsize=true] edge [fontsize=7] rankdir=LR
 
         "unicorn" [shape=record label="unicorn | c | d "];
         "pizza" [shape=record label="one|123|456|1.23"];
         "monkey";
         "root";
-        "l2" [color="green" style="filled" fillcolor="yellow" label="one|123|456|1.23"];
-        "None" [shape="doublecircle" color="orange" style="filled" fillcolor="red"];
-        "piggy";
-        "icecream";
-        "l1";
-        "egg";
-        "unicorn" -> "monkey" ;
-        "root" -> "l1";
-        "root" -> "l2";
-        "root" -> "l2";
-        "root" -> "piggy";
+        "l2" [color="green" style="filled" fillcolor="yellow"
+label="one|123|456|1.23"]; "None" [shape="doublecircle" color="orange"
+style="filled" fillcolor="red"]; "piggy"; "icecream"; "l1"; "egg"; "unicorn" ->
+"monkey" ; "root" -> "l1"; "root" -> "l2"; "root" -> "l2"; "root" -> "piggy";
         "l2" -> "egg";
         "None" -> "root";
         "piggy" -> "unicorn";
@@ -155,30 +146,40 @@ digraph tree{
 */
 string to_dot_string(const shared_ptr<Node> &root, const string &node_shape) {
   string s = "digraph tree{\n";
-  s += string_format(
-      "\tnode [shape=%s margin=0 fontcolor=blue fontsize=9 width=0.5 style=filled]"
-      "\n\tedge [fontsize=8]"
-      "\n\trankdir=LR\n\n",
-      node_shape.c_str());
+  s += string_format("\tnode [shape=%s margin=0 fontcolor=blue fontsize=9 "
+                     "width=0.5 style=filled]"
+                     "\n\tedge [fontsize=8]"
+                     "\n\trankdir=LR\n\n",
+                     node_shape.c_str());
   unordered_set<string> node_names;
   unordered_map<string, VS> successors;
   double max_interval = -2147483648.0;
   string max_leaf_node_name = root->name;
   __dfs(root, node_names, successors, max_leaf_node_name, max_interval);
   for (const auto &e : node_names) {
-    s += string_format("\t\"%s\"", e.c_str());
-    if (e.substr(0,4) == "None") {
-      s += " [shape=\"doublecircle\" color=\"orange\" style=\"filled\" "
+    if (e.substr(0, 4) == VNODE_NAME) {
+      s += "\t\"+\" [shape=\"doublecircle\" color=\"orange\" style=\"filled\" "
            "fillcolor=\"green\"]";
-    } else if (e == max_leaf_node_name) {
-      s += " [shape=\"doublecircle\" color=\"green\" style=\"filled\" "
-           "fillcolor=\"yellow\"]";
+    } else {
+      s += string_format("\t\"%s\"", e.c_str());
+      if (e == max_leaf_node_name) {
+        string label = string_format("[shape=record color=green style=filled "
+                                     "fillcolor=yellow label=\"%s | "
+                                     "%.6f \"]",
+                                     max_leaf_node_name.c_str(), max_interval);
+        s += label;
+      }
     }
+
     s += ";\n";
   }
   for (const auto &e : successors) {
-    for (const auto &v : e.second)
-      s += string_format("\t\"%s\" -> \"%s\";\n", e.first.c_str(), v.c_str());
+    for (const auto &v : e.second) {
+      if (e.first.substr(0, 4) == VNODE_NAME) {
+        s += string_format("\t\"+\" -> \"%s\";\n", v.c_str());
+      } else
+        s += string_format("\t\"%s\" -> \"%s\";\n", e.first.c_str(), v.c_str());
+    }
   }
   s += "}";
   return s;
